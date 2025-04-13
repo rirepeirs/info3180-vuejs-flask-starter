68143eab02eee11fb4e8701cb716946e8e9d2af7
rirepeirs <rirepeirs@gmail.com>
2025-04-13 14:39:50 -0500

    Built form, added database migrations and models

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
 # Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import InputRequired
+
+class MovieForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    poster = FileField('Poster', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'ONly image files are allowed!')])
--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
